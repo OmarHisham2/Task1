68fae95d17344dfcf9fc7e1f690675f098ed8e26
Omar <Omar.Hisham.Gouda@outlook.com>
2022-08-04 13:32:07 +0200

    Changed the values of the 2 numbers

--- src/Task1.kt
+++ src/Task1.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val number1 = 20
-    val number2 = 30
+    val number1 = 50
+    val number2 = 50
     val sum = number1 + number2
     //Val is used because the numbers will not be modified.
     println(sum)
